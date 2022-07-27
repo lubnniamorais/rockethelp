964a1eedfbd856b82892e68019bba7514ad8ecc5
Lubnnia <lubnnia@gmail.com>
2022-07-26 23:19:00 -0300

    Routes configuration RocketHelp

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,9 @@
-import {Center, Spinner} from 'native-base';
+import { Center, Spinner } from 'native-base';
 
-export function Loading(){
-    return(
-        <Center flex={1} bg="gray.700">
-            <Spinner color="secondary.700"></Spinner>
-        </Center>
-    )
+export function Loading() {
+  return (
+    <Center flex={1} bg="gray.700">
+      <Spinner color="secondary.700" />
+    </Center>
+  );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,52 +1,81 @@
 import { useState } from 'react';
 import auth from '@react-native-firebase/auth';
-import {Alert} from 'react-native';
+import { Alert } from 'react-native';
 
-import {VStack, Heading, Icon, useTheme} from 'native-base';
-import {Envelope, Key} from 'phosphor-react-native';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
 
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 
-export function SignIn(){
+export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const { colors } = useTheme();
 
-    const {colors} = useTheme();
-
-    function handleSignIn(){
-        if(!email || !password) {
-            return Alert.alert('Entrar', 'Informe e-mail e senha.');
-        }
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert('Entrar', 'Informe e-mail e senha.');
     }
+    setIsLoading(true);
 
-    return (
-        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-            <Logo />
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        console.log(error);
+        setIsLoading(false);
 
-            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-                Acesse sua conta
-            </Heading>
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('Entrar', 'E-mail inválida.');
+        }
 
-            <Input
-                mb={4}
-                placeholder="Email"
-                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-                onChangeText={setEmail}
-            />
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+        }
 
-            <Input
-                mb={8}
-                placeholder="Senha"
-                InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
-                secureTextEntry
-                onChangeText={setPassword}
-            />
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+        }
 
-            <Button title="Entrar" w="full" onPress={handleSignIn} />
-        </VStack>
-    )
+        return Alert.alert('Entrar', 'Não foi possível acessar.');
+      });
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        mb={4}
+        placeholder="Email"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
+
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
+    </VStack>
+  );
 }
